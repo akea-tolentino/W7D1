891198aee8e09bfa8b988720f3d5b44d118b9184
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-08 17:15:41 -0700

    defined project views and session controller

--- practice-for-ch-rails-ninety-nine-cats2-long-practice/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice/app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+    before_action :require_logged_out, only: [:new, :create]
+    before_action :require_logged_in, only: [:destroy]
 
     def new
         @user = User.new
@@ -7,11 +9,17 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-        @user.reset_session_token!
-        session[:session_token] = @user.session_token
-        redirect_to cats_url
+        
+        if @user
+            login!
+            redirect_to cats_url
+        else
+            render :new
+        end
     end
 
     def destroy
+        logout!
+        redirect_to new_session_url
     end
 end
